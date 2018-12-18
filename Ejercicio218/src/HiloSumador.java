d2143b28d92342cc210f985f4000e1876e6c83f4
carmar04 <2dam.csoro@institutoserpis.org>
2018-12-18 11:23:34 +0100

    Añadido ejercicio 218 semaforos

--- Ejercicio218/src/HiloSumador.java
+++ Ejercicio218/src/HiloSumador.java
@@ -1,27 +1,32 @@
+import java.util.concurrent.Semaphore;
 
 public class HiloSumador extends Thread {
 
 	private String nombre;
 	private int numero;
 	private Contador contador;
-	public HiloSumador(String nombre, Contador contador, int numero) {
+	private Semaphore sem;
+	public HiloSumador(String nombre, Contador contador, int numero, Semaphore sem) {
 		super(nombre);
 		this.contador = contador;
 		this.numero = numero;
+		this.sem = sem;
 	}
 	public void run() {
-		synchronized (contador) {
-			System.out.println("Hilo en ejecución: " + this.getName());
-			for(int i = 0;i < this.numero;i++) {
-//				try {
-//					Thread.sleep(1);
-//				} catch (InterruptedException e) {
-//					System.out.println("Error al hacer esperar al hilo");
-//					e.printStackTrace();
-//				}
-				contador.incrementa();
-			}
-			System.out.println("Resultado del hilo en ejecución: " + this.getName() + " : " + this.contador.mostrarValor());
+		System.out.println("Hilo en ejecución: " + this.getName());
+		
+	try {
+			sem.acquire();
+		for(int i = 0;i < this.numero;i++) {
+			Thread.sleep(1);
+			contador.incrementa();
 		}
+		sem.release();
+	} catch (InterruptedException e) {
+			System.out.println("Error al hacer esperar al hilo");
+			e.printStackTrace();
+	}
+			
+		System.out.println("Resultado del hilo en ejecución: " + this.getName() + " : " + this.contador.mostrarValor());
 	}
 }
--- Ejercicio218/src/LanzaHilos216.java
+++ Ejercicio218/src/LanzaHilos216.java
@@ -1,14 +1,18 @@
+import java.util.concurrent.Semaphore;
 
 public class LanzaHilos216 {
 
 	public static void main(String[] args) {
 		Contador contador = new Contador(100);
+		Semaphore sem = new Semaphore(1);
 		
-		HiloSumador hiloSumador = new HiloSumador("Hilo sumador", contador, 300);
-		HiloRestador hiloRestador = new HiloRestador("Hilo Restador", contador, 300);
+		HiloSumador hiloSumador = new HiloSumador("Hilo sumador", contador, 300, sem);
+		HiloRestador hiloRestador = new HiloRestador("Hilo Restador", contador, 300, sem);
+		HiloSumador hiloSumador2 = new HiloSumador("Hilo sumador 2", contador, 200, sem);
 		
 		hiloSumador.start();
 		hiloRestador.start();
+		hiloSumador2.start();
 		
 		try {
 			hiloSumador.join();
@@ -17,7 +21,6 @@ public class LanzaHilos216 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		
 		System.out.println("Valor del contador: " + contador.mostrarValor());
 		System.out.println("Programa finalizado");
